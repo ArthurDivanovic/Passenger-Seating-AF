22f2d4e2c8b2c14215fb62f802fca7345b7349cb
Arthur Divanovic <arthur.divanovic@polytechnique.edu>
2023-01-24 16:04:37 +0100

    Began implementation of constraints.py

--- constraints.py
+++ constraints.py
@@ -1,10 +1,34 @@
 import pandas as pd
 import numpy as np
 from pulp import *
-#from planes import *
+from planes import *
 from passengers import *
 
 
-def create_constraintes(passengers, plane):
-    return None
+def create_constraints(passengers, plane):
 
+    # Problem
+    prob = LpProblem("Passenger Seating Problem", LpMinimize)
+
+    # Variables
+    p = len(passengers.passengers)
+    s = plane.nb_seat
+    x = LpVariable.dict("x[i,j]", (range(1, s+1), range(1, p+1)), 0, 1)
+    Y = dict()
+    for group in passengers.bounds.keys():
+        first_element = passengers.bounds[group][0]
+        last_element = passengers.bounds[group][1]
+        Y[group] = LpVariable.dict("y"+str(group)+"[i,j]", (range(first_element, last_element+1), range(first_element, last_element+1)), 0, None)
+
+    # Objective function
+    for group in Y.keys():
+        y = Y[group]
+        first_element = passengers.bounds[group][0]
+        last_element = passengers.bounds[group][1]
+        group_size = np.array(y).shape[0]
+        prob += lpSum([y[i][j] for i in range(first_element, last_element+1) for j in range(first_element, last_element+1)]) / 2
+
+    # Add constraints
+
+
+    return None
